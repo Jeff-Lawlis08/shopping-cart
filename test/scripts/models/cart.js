3eeacd87ce3cb7f3b1110bc6eff41ec892e73c3f
Jeff Lawlis <jeff.lawlis@yahoo.com>
2016-11-15 18:05:58 -0600

    added more method testing

--- test/scripts/models/cart.js
+++ test/scripts/models/cart.js
@@ -9,15 +9,17 @@ beforeEach(()=>{
 cart = new Cart();
 });
 
+//model functionality
 describe('cart model', ()=>{
   it('should be a function', ()=>{
     expect(Cart).to.be.a('function');
   });
 
-  it('should add items to the cart', ()=>{
+  it('should create a backbone model', ()=>{
     expect(cart).to.be.an.instanceof(Backbone.Model);
   });
 
+//defaults
   it('should have a default total of 0', ()=>{
     expect(cart.get('total')).to.equal(0);
   });
@@ -26,10 +28,15 @@ describe('cart model', ()=>{
     expect(cart.get('items')).to.be.an('array');
     expect(cart.get('items')).to.deep.equal([]);
   });
-
+//methods
   it('should have a function addItem', ()=>{
     expect(cart).to.have.property('addItem');
     expect(cart.addItem).to.be.a('function');
+  });
+
+  it('should add an item to the items array when running addItem',()=>{
+    cart.addItem({name:'Jeff',age:26});
+    expect(cart.get('items')).to.deep.equal([{name:'Jeff',age:26}]);
   });
 
   it('should fire a change event when addItem is called', ()=>{
@@ -44,6 +51,12 @@ describe('cart model', ()=>{
     expect(cart.removeItem).to.be.a('function');
   });
 
+  it('should remove an item when running removeItem', ()=>{
+    cart.addItem('thing');
+    cart.removeItem('thing');
+    expect(cart.get('items')).to.deep.equal([]);
+  });
+
   it('should fire a change event when removeItem is called', ()=>{
     let spy = sinon.spy();
     cart.addItem('hey');
@@ -55,6 +68,11 @@ describe('cart model', ()=>{
   it('should have a function getTotal', ()=>{
     expect(cart).to.have.property('getTotal');
     expect(cart.getTotal).to.be.a('function');
+  });
+
+  it('should get the total price of the items when running addItem', ()=>{
+    cart.addItem({name: 'jeff', price: 5000});
+    expect(cart.get('total')).to.equal(5000);
   });
 
   it('should not fire a change event when getTotal is called', ()=>{
